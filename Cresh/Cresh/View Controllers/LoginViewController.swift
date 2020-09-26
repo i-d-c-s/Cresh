90a66f492f91808a6686e970f3f1b8b68bacee45
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-09-26 05:16:36 -0400

    Added tap recognizers to toggle keyboards

--- Cresh/Cresh/View Controllers/LoginViewController.swift
+++ Cresh/Cresh/View Controllers/LoginViewController.swift
@@ -56,14 +56,8 @@ class LoginViewController: UIViewController {
     @IBAction func registerUser(_ sender: Any) {
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toggleKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
     }
-    */
     
 }
