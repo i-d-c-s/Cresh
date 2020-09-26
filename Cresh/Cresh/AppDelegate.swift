51a582308486e8ee09a325707ff6abeb7d8e9460
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-09-26 04:42:15 -0400

    Initialized Parse server with app

--- Cresh/Cresh/AppDelegate.swift
+++ Cresh/Cresh/AppDelegate.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+            configuration.applicationId = "cresh"
+            configuration.server = "https://cresh.herokuapp.com/parse"
+                    })
+                )
+        
         return true
     }
 
