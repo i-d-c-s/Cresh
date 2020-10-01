cf55817d41b8ee4bbfba48d92fbcd94d1915946d
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-10-01 04:35:26 -0400

    Created relevant files for WorkOutScreen

---
+++ Cresh/Cresh/Views/WorkoutsCell.swift
@@ -0,0 +1,13 @@
+//
+//  WorkoutsCell.swift
+//  Cresh
+//
+//  Created by Subomi Popoola on 10/1/20.
+//  Copyright © 2020 Subomi Popoola. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutsCell: UICollectionViewCell {
+    
+}
