ec946e47e2c1dccb83a6bb7d721fabe59bf5a347
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-10-06 08:55:20 -0400

    Created GroupDetails UI along with its relevant files

---
+++ Cresh/Cresh/Views/GroupDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupDetailCell.swift
+//  Cresh
+//
+//  Created by Subomi Popoola on 10/6/20.
+//  Copyright © 2020 Subomi Popoola. All rights reserved.
+//
+
+import UIKit
+
+class GroupDetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
