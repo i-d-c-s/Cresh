79d01fb20e546970a918dd87263154a80d3f53d0
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-10-12 08:12:45 -0400

    Added Constraints

--- Cresh/Cresh/View Controllers/SignUpViewController.swift
+++ Cresh/Cresh/View Controllers/SignUpViewController.swift
@@ -88,15 +88,8 @@ class SignUpViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    @IBAction func onTap(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+ 
 }
