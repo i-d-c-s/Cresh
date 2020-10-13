ad836c4a32a406e73d3550124d8cf97a5054e895
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-10-13 17:24:24 -0400

    Populating profile screen

--- Cresh/Cresh/View Controllers/ProfileViewController.swift
+++ Cresh/Cresh/View Controllers/ProfileViewController.swift
@@ -39,41 +39,53 @@ class ProfileViewController: UIViewController {
             self.profileImage.file = (imageData as! PFFileObject)
             self.profileImage.loadInBackground()
         }
-        self.usernameLabel.text = String(format: "@%s", user.username)
+        self.usernameLabel.text = String(format: "@%s", user.username!)
         var lost = user.object(forKey: "Lost") as? Int
         if lost  == nil {
             lost = 0
         }
-        self.numLossesLabel.text = String(format: "Lost: %d", lost)
+        self.numLossesLabel.text = String(format: "Lost: %d", lost!)
         var wins = user.object(forKey: "Lost") as? Int
         if wins == nil {
             wins = 0
         }
-        self.numWinsLabel.text = String(format: "Won: %d", wins)
-        self.numChallengesLabel.text = String(format: "Challenges: %d", wins + lost)
-        var school = user.object(forKey: "School") as? String
+        self.numWinsLabel.text = String(format: "Won: %d", wins!)
+        self.numChallengesLabel.text = String(format: "Challenges: %d", wins! + lost!)
+        let school = user.object(forKey: "School") as? String
         if school == nil{
             self.schoolLabel.alpha = 0
         } else {
             self.schoolLabel.text = school
         }
-        var gender = user.object(forKey: "Gender") as? String
+        let gender = user.object(forKey: "Gender") as? String
         if gender == nil {
             self.genderLabel.alpha = 0
         } else {
             self.genderLabel.text = gender
         }
-        var squats = user.object(forKey: "squats") as? Int
+        let squats = user.object(forKey: "squats") as? Int
         if squats == nil {
             self.numSquatsLabel.text = "Squats: 0"
         } else {
-            self.numSquatsLabel.text = String(format: "Squats: %d", squats)
+            self.numSquatsLabel.text = String(format: "Squats: %d", squats!)
         }
-        var pushUps = user.object(forKey: "pushUps") as? Int
+        let pushUps = user.object(forKey: "pushUps") as? Int
         if pushUps == nil {
             self.numPushupLabel.text = "PushUps: 0"
         } else {
-            self.numSquatsLabel.text = String(format: "PushUps: %d", pushUps)
+            self.numSquatsLabel.text = String(format: "PushUps: %d", pushUps!)
+        }
+        let inclinePushUps = user.object(forKey: "inclinePushUps") as? Int
+        if inclinePushUps == nil {
+            self.numInclinePushupLabel.text = "Incline PushUps: 0"
+        } else {
+            self.numInclinePushupLabel.text = String(format: "Incline PushUps: %d", inclinePushUps!)
+        }
+        let declinePushUps = user.object(forKey: "declinePushUps") as? Int
+        if declinePushUps == nil {
+            self.numDeclinePushupLabel.text = "Decline PushUps: 0"
+        } else {
+            self.numDeclinePushupLabel.text = String(format: "Decline PushUps: %d", declinePushUps!)
         }
     }
     
@@ -93,16 +105,5 @@ class ProfileViewController: UIViewController {
             }
         }
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
